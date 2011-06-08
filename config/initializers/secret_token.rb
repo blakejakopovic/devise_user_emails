9842edd9e91fb9d1c3534e6eb3885775c69a7ae2
Blake Jakopovic <wakoinc@gmail.com>
2011-06-09 09:35:46 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseUserEmails::Application.config.secret_token = 'f319ffcfc18bc795409d4007f2594eda4d8d512fb90469f8758fac1e4bb6dcae4a616ec74c0aea1c7ef422824f159eb0dcf8cfc05b1439dad9e5ceec8bd83a7a'
